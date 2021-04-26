7a854b1a8f21c64a4958bcc73cf0420ec2249f57
kode21 <bryantmejia21@gmail.com>
2021-04-26 14:42:57 -0400

    Eliminando atributos y metodo constructor

--- clases/contact.php
+++ clases/contact.php
@@ -1,28 +1,20 @@
 <?php 
     require_once("conexion.php");
     class Contactos{
-        private string $nombreContacto;
-        private string $emailContacto;
-        private string $telefono;
-        function __construct($nombreContacto, $emailContacto, $telefono){
-            $this->nombreContacto = $nombreContacto;
-            $this->emailContacto = $emailContacto;
-            $this->telefono = $telefono;
-        }
 
         /*
             SCOPE_IDENTITY() devuelve el ultimo identity generado en el mismo ambito.   
         */
 
-        public function crearContacto($userId){
+        public function crearContacto($userId, $nombreContacto, $emailContacto, $telefono){
             $conect = new ConexionSQLSERVER();
             $query = 
-            "INSERT INTO CONTACTO(NOMBRE_CONTACTO,ID_USUARIO) VALUES ('$this->nombreContacto',
+            "INSERT INTO CONTACTO(NOMBRE_CONTACTO,ID_USUARIO) VALUES ('$nombreContacto',
             '$userId')
             DECLARE @ultimoID INT 
             SELECT @UltimoID = SCOPE_IDENTITY()
-            INSERT INTO TELEFONO(ID_CONTACTO,TELEFONO_CONTACTO,TIPO) VALUES (@ultimoID,'$this->telefono','Casa')   
-            INSERT INTO EMAIL(ID_CONTACTO,EMAIL_CONTACTO) VALUES (@ultimoID,'$this->emailContacto') ";
+            INSERT INTO TELEFONO(ID_CONTACTO,TELEFONO_CONTACTO,TIPO) VALUES (@ultimoID,'$telefono','Casa')   
+            INSERT INTO EMAIL(ID_CONTACTO,EMAIL_CONTACTO) VALUES (@ultimoID,'$emailContacto') ";
             $contacto = $conect->ejecutarQuery($query);
             return $contacto;
         }
