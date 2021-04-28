e389e65d6b100209a3861bc87610aad116f51dbd
kode21 <bryantmejia21@gmail.com>
2021-04-27 22:52:36 -0400

    agregando estilos css

--- home.php
+++ home.php
@@ -10,9 +10,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agenda Telefonica</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
+    <link rel="stylesheet" href="css/home.css">
 </head>
 <body>
-    <?php 
+    <main class="main">
+        <?php 
         require_once("clases/contact.php");
         $idUsuario =  $_SESSION['id']['ID_USUARIO'];
         $objContactos = new Contactos();
@@ -20,39 +23,49 @@
         // echo '<pre>';
         // print_r($contactos);
         // echo '</pre>';
-        echo "<table>";
-        echo "<thead>";
-        echo "<tr>";
-        echo "<th>Nombre</th>";
-        echo "<th>Telefono</th>";
-        echo "<th>Tipo</th>";
-        echo "<th>Telefono 2</th>";
-        echo "<th>Tipo</th>";
-        echo "<th>Telefono 3</th>";
-        echo "<th>Tipo</th>";
-        echo "<th>Email</th>";
-        echo "</tr>";
-        echo "</thead>";
-        echo "<tbody>";
             foreach ($contactos as $key => $contacto) {
+                echo "<div class='card '>";
                 $contador = 0;
-                echo "<tr>";
-                echo "<td>".$contacto['NOMBRE_CONTACTO'][0]['NOMBRE_CONTACTO']."</td>";
-                while($contador < count($contacto['TELEFONO_CONTACTO'])){
-                    echo "<td>".$contacto['TELEFONO_CONTACTO'][$contador]['TELEFONO_CONTACTO']."</td>";
-                    echo "<td>".$contacto['TELEFONO_CONTACTO'][$contador]['TIPO']."</td>";
+                echo "<div class='card-header'>";
+                echo "<div class='card-header-title'> ".$contacto['NOMBRE_CONTACTO'][0]['NOMBRE_CONTACTO']."</div>"; 
+               echo "</div>";
+               echo "<div class='card-content'>";
+               echo "<div class='content'> ";
+                do{
+                    echo "<div class='content-text'>";
+                    echo "<span class='card-text' >".$contacto['TELEFONO_CONTACTO'][$contador]['TIPO']." :</span>";
+                    echo "<span class='card-text'>".$contacto['TELEFONO_CONTACTO'][$contador]['TELEFONO_CONTACTO']."</span>";
+                    echo "</div>";
                     $contador++;
-                }
-                echo "<td>".$contacto['EMAIL_CONTACTO'][0]['EMAIL_CONTACTO']."</td>";
-                echo "</tr>";
+                }while($contador < count($contacto['TELEFONO_CONTACTO']));
+                echo "<div class='content-text'>";
+               echo "<span class='card-text'>Email :</span>";
+               echo "<span class='card-text'>".$contacto['EMAIL_CONTACTO'][0]['EMAIL_CONTACTO']."</span>";
+               echo "</div>";
+               echo "</div>";
+               echo "</div>";
+               echo "<footer class='card-footer'>";
+               echo "<a class='card-footer-item' href='?action=edit&id=".$contacto['ID_CONTACTO'][0]."'>Editar</a>";
+               echo "<a class='card-footer-item' href='?action=delete&id=".$contacto['ID_CONTACTO'][0]."'>Borrar</a>";
+               echo "</footer>";
+               echo "</div>";
+               echo "</div>";
+               echo "</div>";
             }
-
-        echo "</tbody>";
-        echo"</table>";
     ?>
-    <form action="logout.php" method="POST">
-        <input type="submit" value="Cerrar Sesion" name="logOut">
+    </main>
+    <form action="logout.php" method="POST" class="logout">
+        <input type="submit" value="Cerrar Sesion" name="logOut" class="button is-dark">
     </form>
 </body>
 </html>
 
+<?php 
+    // while($contador < count($contacto['TELEFONO_CONTACTO'])){
+    //     echo "<td>".$contacto['TELEFONO_CONTACTO'][$contador]['TELEFONO_CONTACTO']."</td>";
+    //     echo "<td>".$contacto['TELEFONO_CONTACTO'][$contador]['TIPO']."</td>";
+    //     $contador++;
+    // }
+    // echo "<td>".$contacto['EMAIL_CONTACTO'][0]['EMAIL_CONTACTO']."</td>";
+    // echo "</tr>";
+?>
--- index.php
+++ index.php
@@ -12,13 +12,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
+    <link rel="stylesheet" href="css/login.css">
     <title>Agenda Telefonica</title>
 </head>
 <body>
-    <form action="login.php" method="POST">
-        <input type="email" placeholder="Email" name="correo">
-        <input type="password" placeholder="Contraseña" name="clave">
-        <input type="submit" value="Iniciar Sesion">
+    <form action="login.php" method="POST" class="login">
+        <div class="field">
+            <label for="email" class="label">Email</label>
+            <div class="control">
+                <input type="email" id="email" class="input" placeholder="Email" name="correo">
+            </div>
+        </div>
+        <div class="field">
+            <label for="password" class="label">Contraseña</label>
+            <div class="control">
+                <input type="password" class="input" id="password" placeholder="Contraseña" name="clave">
+            </div>
+        </div>
+        <input type="submit" value="Iniciar Sesion" class="button is-light  is-link">
     </form>
 </body>
 </html>
