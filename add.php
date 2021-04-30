7963fffe71aadfe39e0b78d5396fbf7ce8ee210a
kode21 <bryantmejia21@gmail.com>
2021-04-30 15:28:18 -0400

    archivo que se encarga de la creacion de contactos

---
+++ add.php
@@ -0,0 +1,23 @@
+<?php 
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+      require_once("clases/contact.php");  
+      require_once("clases/sesion.php");
+      Sesion::VerificarSesion();
+      $idUsuario = $_SESSION['id']['ID_USUARIO'];
+        if(
+            isset($_POST['nombre']) && 
+            isset($_POST['email']) && 
+            isset($_POST['telefono'])&&
+            isset($_POST['type'])){
+                $regexTelefono = '/(809|829)-\d{3}-\d{4}/';
+                $nombreContacto = $_POST['nombre'];
+                $emailContacto = $_POST['email'];
+                $telefono = $_POST['telefono'];
+                $tipo = $_POST['type'];
+                
+                $objContacto = new Contactos();
+                $objContacto->crearContacto($idUsuario,$nombreContacto,$emailContacto,$telefono, $tipo);
+                header("Location:home.php");
+            }
+    }
+?>
