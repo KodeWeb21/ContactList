947c6d1bd6210f06d35f12b264d036704924160a
kode21 <bryantmejia21@gmail.com>
2021-05-04 16:10:10 -0400

    manejador para editar los contacto

---
+++ edit.php
@@ -0,0 +1,27 @@
+<?php 
+    require_once("clases/contact.php");
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if(isset($_POST['nombre']) &&
+           !empty($_POST['nombre']) &&
+           isset($_POST['email']) &&
+           !empty($_POST['email']) &&
+           isset($_POST['telefono']) &&
+           !empty($_POST['telefono']) &&
+           isset($_POST['type']) &&
+           !empty($_POST['type']) &&
+           isset($_POST['id']) &&
+           !empty($_POST['id'])){
+               $nombre = $_POST['nombre'];
+               $email = $_POST['email'];
+               $telefono = $_POST['telefono'];
+               $tipo = $_POST['type'];
+                $idContacto = $_POST['id'];
+
+               $contacto = new Contactos();
+               $contacto->editarNombreContacto($nombre,$idContacto);
+               $contacto->editarEmailContacto($email,$idContacto);
+               $contacto->editarTelefonoContacto($telefono, $tipo, $idContacto);
+               header("Location:home.php");
+               exit();
+           }
+    }
