8219da686b931fc67d292e542c3aa1d1718186ac
kode21 <bryantmejia21@gmail.com>
2021-07-06 18:19:53 -0400

    validacion al agregar contactos

--- contacto/add.php
+++ contacto/add.php
@@ -8,7 +8,12 @@
             isset($_POST['nombre']) && 
             isset($_POST['email']) && 
             isset($_POST['telefono'])&&
-            isset($_POST['type'])){
+            isset($_POST['type']) &&
+            !empty($_POST['nombre']) &&
+            !empty($_POST['email']) &&
+            !empty($_POST['telefono'])&&
+            !empty($_POST['type']) &&
+            isset($_POST['crear'])){
                 $regexTelefono = '/(809|829)-\d{3}-\d{4}/';
                 $nombreContacto = $_POST['nombre'];
                 $emailContacto = $_POST['email'];
@@ -18,5 +23,7 @@
                 $objContacto = new Contactos();
                 $objContacto->crearContacto($idUsuario,$nombreContacto,$emailContacto,$telefono, $tipo);
                 header("Location:../my/home.php");
+            }else{
+              header("Location:../my/home.php");
             }
     }
