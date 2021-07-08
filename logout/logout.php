ca2e4386e08d64876eaf33b2193dd7243f3bcb7c
kode21 <bryantmejia21@gmail.com>
2021-07-07 22:42:47 -0400

    logica del logout

--- logout/logout.php
+++ logout/logout.php
@@ -1,8 +1,8 @@
 <?php 
-     require_once("clases/sesion/sesion.php");
-     require_once("clases/redirect/redirect.php");
-   if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    if(isset($_POST['logOut'])){
+     require_once("../clases/sesion/sesion.php");
+     require_once("../clases/redirect/redirect.php");
+   if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    if(isset($_GET['sesion'])){
         Sesion::cerrarSesion();
     }
    }else{
