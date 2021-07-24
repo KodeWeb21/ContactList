b9c9b37c1a982dd3d687b898b037bd1e5f9b879d
kode21 <bryantmejia21@gmail.com>
2021-07-24 13:58:54 -0400

    agregando mensaje de usuario no existente

--- login/login.php
+++ login/login.php
@@ -10,7 +10,27 @@
             $user = $objLogin->logearUsuario($correoUsuario,$clave);
             
             if(!$user){
-                echo 'El usuario no existe';
+                echo "
+                <style>
+                body{
+                    display:grid;
+                    place-items:center;
+                    min-height:100vh;
+                    background-color:#535c68;
+                  }
+                  
+                  .message{
+                    background:linear-gradient(to bottom left, rgba(255,0,0,.2),rgba(255,0,0,1));
+                    min-height:50px;
+                    display:flex;
+                    align-items:center;
+                    padding:1rem;
+                    color:#f5f6fa;
+                    font-weight:bold;
+                  }
+                </style>
+                <div class='message'>El usuario no existe</div>
+                ";
                 exit();
             }
     
